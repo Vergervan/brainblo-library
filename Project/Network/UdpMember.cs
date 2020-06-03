5fdf6e0e0a34f985dcf49731133f08a328c817cd
Vergervan <krazy,vovan@mail.ru>
2020-06-03 15:00:41 +0300

    Made a skeleton for SendOption and WaitResponse functions

--- Project/Network/UdpMember.cs
+++ Project/Network/UdpMember.cs
@@ -6,13 +6,22 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Net;
+using System.Timers;
+using System.Reflection;
+using Timer = System.Timers.Timer;
 
 namespace BrainBlo.Network
 {
+    public enum SendOption
+    {
+        None = 0,
+        FullPacket = 1
+    }
     public class UdpMember
     {
         private Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
         public Socket Socket { get { return socket; } private set { } }
+        private SendOption _sendOption = SendOption.None;
         public event EventHandler OnDispose; 
         public IPEndPoint EndPoint { get; private set; }
         private int maxBufferSize = 1024;
@@ -30,6 +39,7 @@ namespace BrainBlo.Network
         private readonly object lockPMlist = new object();
         private Queue<OnReceiveMessage> onReceiveMessages = new Queue<OnReceiveMessage>();
         private AutoResetEvent waitHandler = new AutoResetEvent(false);
+        private AutoResetEvent waitResponse = new AutoResetEvent(false);
         public int PendingMessagesCount { get { return pendingMessages.Count; } private set { } }
         public int ReceiveMessagesCount { get { return onReceiveMessages.Count; } private set { } }
 
@@ -52,7 +62,7 @@ namespace BrainBlo.Network
                 socket.Connect(EndPoint);
             }catch(Exception e)
             {
-                
+                log.Write($"{this} {MethodBase.GetCurrentMethod()}: {e.Message}");
             }
             isWorking = true;
 
@@ -105,8 +115,9 @@ namespace BrainBlo.Network
                     messageSize = socket.ReceiveFrom(buffer, ref clientEndPoint);
                 }catch(Exception e)
                 {
-                    log.Write(e.Message);
-                    continue;
+                    log.Write($"{this} {MethodBase.GetCurrentMethod()}: {e.Message}");
+                    Stop();
+                    return;
                 }
                 IPEndPoint cleanEP = (IPEndPoint)clientEndPoint;
                 byte[] messageBuffer = Buffer.ChangeBufferSize(buffer, 0, messageSize);
@@ -143,6 +154,10 @@ namespace BrainBlo.Network
                         }
                     }
                 }
+                else if(code == 3)
+                {
+
+                }
             }
         }
 
@@ -159,8 +174,9 @@ namespace BrainBlo.Network
                     messageSize = socket.ReceiveFrom(buffer, ref clientEndPoint);
                 }catch(Exception e)
                 {
-                    log.Write(e.Message);
-                    continue;
+                    log.Write($"{this} {MethodBase.GetCurrentMethod()}: {e.Message}");
+                    Stop();
+                    return;
                 }
                 IPEndPoint cleanEP = (IPEndPoint)clientEndPoint;
                 byte[] messageBuffer = Buffer.ChangeBufferSize(buffer, 0, messageSize);
@@ -213,13 +229,19 @@ namespace BrainBlo.Network
             waitHandler.Reset();
             return orm.Message;
         }
+        //TODO Response mechanism
+        public void WaitResponse(IPEndPoint point)
+        {
 
-        public void Send(byte[] message) { Send(EndPoint, message); }
+        }
 
-        public void Send(IPEndPoint point, byte[] message)
+        public void Send(byte[] message) { Send(EndPoint, message, SendOption.None); }
+        public void Send(IPEndPoint point, byte[] message) { Send(point, message, SendOption.None); }
+
+        public void Send(IPEndPoint point, byte[] message, SendOption sendOption)
         {
             int bufferSize = message.Length;
-            int lastIndex = 0;
+            
             byte[] byteLength = new byte[4];
             string str = string.Format($"{message.Length}");
             for (int i = byteLength.Length - str.Length, j = 0; i < byteLength.Length; i++, j++)
@@ -232,26 +254,36 @@ namespace BrainBlo.Network
             }
             catch (Exception e)
             {
-                log.Write(e.Message);
+                log.Write($"{this} {MethodBase.GetCurrentMethod()}: {e.Message}");
                 return;
             }
 
-            while (bufferSize > 0)
-            {
-                byte[] messagePartBuffer = Buffer.ChangeBufferSize(message, lastIndex, bufferSize < maxBufferSize ? bufferSize : maxBufferSize - 1);
-                try
-                {
-                    socket.SendTo(Buffer.AddStartCode(messagePartBuffer, 1), point);
-                }catch(Exception e)
-                {
-                    log.Write(e.Message);
-                    return;
-                }
-                bufferSize -= maxBufferSize - 1;
-                if (bufferSize < 0) bufferSize = 0;
-                lastIndex += maxBufferSize - 1;
+            switch (sendOption) {
+                case SendOption.None:
+                    int lastIndex = 0;
+                    while (bufferSize > 0)
+                    {
+                        byte[] messagePartBuffer = Buffer.ChangeBufferSize(message, lastIndex, bufferSize < maxBufferSize ? bufferSize : maxBufferSize - 1);
+                        try
+                        {
+                            socket.SendTo(Buffer.AddStartCode(messagePartBuffer, 1), point);
+                        } catch (Exception e)
+                        {
+                            log.Write($"{this} {MethodBase.GetCurrentMethod()}: {e.Message}");
+                            return;
+                        }
+                        bufferSize -= maxBufferSize - 1;
+                        if (bufferSize < 0) bufferSize = 0;
+                        lastIndex += maxBufferSize - 1;
+                    }
+                    break;
+                case SendOption.FullPacket:
+                    socket.SendTo(Buffer.AddStartCode(message, 1), point);
+                    break;
             }
         }
+
+        
 
         private bool PendingMessageInList(IPEndPoint point)
         {
