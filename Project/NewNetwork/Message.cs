f69de673669219faec65024fcae75e2b544baac8
Vergervan <krazy,vovan@mail.ru>
2020-10-22 00:10:02 +0300

    Move Message class from NetHandle into a separate file with the same name

---
+++ Project/NewNetwork/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+
+namespace BrainBlo.NewNetwork
+{
+    public class Message
+    {
+        public byte[] messageBuffer { get; private set; }
+        public int messageSize { get; private set; }
+        public IPEndPoint point { get; private set; }
+
+        public Message(byte[] messageBuffer, int messageSize, IPEndPoint point)
+        {
+            this.messageBuffer = messageBuffer;
+            this.messageSize = messageSize;
+            this.point = point;
+        }
+        public Message(byte[] messageBuffer, IPEndPoint point) : this(messageBuffer, messageBuffer.Length, point) { }
+    }
+}
